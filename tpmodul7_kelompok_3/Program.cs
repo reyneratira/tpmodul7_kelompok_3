c01bc1db01e434f148fdd17bd1f5a0955aa55ff3
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-04-10 18:52:22 +0700

    menambahkan class DataMahasiswa2311104057

--- tpmodul7_kelompok_3/Program.cs
+++ tpmodul7_kelompok_3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+public class DataMahasiswa2311104057
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        string path = "tp7_1_2311104057.json"; // ganti dengan nama file yang sudah diganti sesuai NIM
+        string jsonContent = File.ReadAllText(path);
+
+        Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonContent);
+
+        Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa2311104057.ReadJSON();
+    }
+}
